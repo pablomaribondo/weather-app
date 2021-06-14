21a8dd9fa13fa8b9657902aaa4fe7e69d6cafae4
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-14 09:05:07 -0300

    feat: make store available in the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from "./App";
+import store from './store/store';
+import '../node_modules/bulma/css/bulma.min.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
